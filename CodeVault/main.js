afc96d3ea89c6c0a3b0910bbde55168ee6e21067
Anirudh A V <anirudh.av02@gmail.com>
2023-05-08 09:46:04 +0530

    React App configured Electron App configured

---
+++ CodeVault/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  })
+
+  win.loadURL('http://127.0.0.1:5173/')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
